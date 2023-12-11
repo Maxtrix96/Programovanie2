c9c33ab7fb9a7ca478516829089ce6203a479075
Maxtrix96 <antonbelica@gmail.com>
2023-12-11 22:45:58 +0100

    a

---
+++ Python/Tkinter/random squares, squares on click.py
@@ -0,0 +1,65 @@
+import tkinter as tk
+import random
+
+MAX_HEIGHT = 700
+MAX_WIDTH = 700
+
+def get_random_hex_color():
+    color = "#{:06x}".format(random.randint(0, 0xFFFFFF))
+    return color
+
+def set_size():
+    min_dimension = min(MAX_WIDTH, MAX_HEIGHT)
+    size = random.randrange(0, int(min_dimension / 15))
+    return size
+
+def draw_square():
+    size = set_size()
+
+    x1 = random.randrange(MAX_HEIGHT - size)
+    y1 = random.randrange(MAX_HEIGHT - size)
+    
+    x2 = min(x1 + size, MAX_HEIGHT)
+    y2 = min(y1 + size, MAX_HEIGHT)
+
+    color = get_random_hex_color()
+    canvas.create_rectangle(x1, y1, x2, y2, fill=color)
+
+def draw_square_on_click(click):
+    size = set_size()
+
+    x1 = click.x
+    y1 = click.y
+    
+    x2 = min(x1 + size, MAX_HEIGHT)
+    y2 = min(y1 + size, MAX_HEIGHT)
+
+    color = get_random_hex_color()
+    canvas.create_rectangle(x1, y1, x2, y2, fill=color)
+
+root = tk.Tk()
+root.title("Random squares test")
+
+canvas = tk.Canvas(root, width=MAX_WIDTH, height=MAX_HEIGHT)
+canvas.pack()
+
+canvas.bind("<Button-1>", draw_square_on_click)
+canvas.bind("<KeyRelease-a>", draw_square) # ????
+'''
+<KeyPress>: Triggered when any key on the keyboard is pressed.
+<KeyRelease>: Triggered when any key on the keyboard is released.
+<KeyPress-A>: Triggered when the 'A' key is pressed. Replace 'A' with the desired key.
+<KeyRelease-A>: Triggered when the 'A' key is released. Replace 'A' with the desired key.
+<Control-KeyPress-a>: Triggered when the 'a' key is pressed while the Control key is held down.
+'''
+
+def draw_20_squares():
+    for i in range(20):
+        draw_square()
+
+button = tk.Button(root, text="Draw 20 squares", command=draw_20_squares)
+button.pack()
+button2 = tk.Button(root, text="Draw a square", command=draw_square)
+button2.pack()
+
+root.mainloop()
