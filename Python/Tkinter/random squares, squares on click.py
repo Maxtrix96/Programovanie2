f0eccfa02c8bb2ba312c9c4f866f2f368f7a4a52
Maxtrix96 <antonbelica@gmail.com>
2023-12-12 23:44:22 +0100

    h

--- Python/Tkinter/random squares, squares on click.py
+++ Python/Tkinter/random squares, squares on click.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 import random
 
-MAX_HEIGHT = 700
-MAX_WIDTH = 700
+MAX_HEIGHT = 600
+MAX_WIDTH = 600
 
 root = tk.Tk()
 root.title("Random squares test")
@@ -16,7 +16,7 @@ def get_random_hex_color():
 
 def set_size():
     minDimension = min(MAX_WIDTH, MAX_HEIGHT)
-    size = random.randrange(0, int(minDimension / 2)) 
+    size = random.randrange(0, int(minDimension / 8)) 
     return size
 
 def draw_square():
@@ -43,7 +43,32 @@ def draw_square_on_click(click):
     color = get_random_hex_color()
     canvas.create_rectangle(x1, y1, x2, y2, fill=color)
 
+def draw_circle():
+    size = set_size()
+
+    x1 = random.randrange(MAX_WIDTH - size)
+    y1 = random.randrange(MAX_HEIGHT - size)
+    
+    x2 = x1 + size
+    y2 = y1 + size
+
+    color = get_random_hex_color()
+    canvas.create_oval(x1, y1, x2, y2, fill=color)
+
+def draw_circle_on_click(click):
+    size = set_size()
+
+    x1 = click.x - size // 2
+    y1 = click.y - size // 2
+    
+    x2 = min(x1 + size, MAX_WIDTH)
+    y2 = min(y1 + size, MAX_HEIGHT)
+
+    color = get_random_hex_color()
+    canvas.create_oval(x1, y1, x2, y2, fill=color)
+
 canvas.bind("<Button-1>", draw_square_on_click)
+canvas.bind("<Button-3>", draw_circle_on_click)
 canvas.bind("<KeyPress - a>", draw_square) # ????
 '''
 <KeyPress>: Triggered when any key on the keyboard is pressed.
@@ -57,9 +82,17 @@ def draw_20_squares():
     for i in range(20):
         draw_square()
 
+def draw_20_circles():
+    for i in range(20):
+        draw_circle()
+
 button = tk.Button(root, text="Draw 20 squares", command=draw_20_squares)
 button.pack()
 button2 = tk.Button(root, text="Draw a square", command=draw_square)
 button2.pack()
+button4 = tk.Button(root, text="Draw 20 circles", command=draw_20_circles)
+button4.pack()
+button3 = tk.Button(root, text="Draw a circle", command=draw_circle)
+button3.pack()
 
 root.mainloop()
