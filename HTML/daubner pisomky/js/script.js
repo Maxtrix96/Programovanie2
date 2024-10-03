99e1f0c7f19d6ee2fa621188cfad7f739a35f8be
Maxtrix96 <antonbelica@gmail.com>
2024-10-03 18:26:05 +0200

    hotovy generator pisomiek pre daubnera

--- HTML/daubner pisomky/js/script.js
+++ HTML/daubner pisomky/js/script.js
@@ -1,21 +1,26 @@
 function ulozitOtazky() {
-    const vlozenePisomky = document.getElementById("zadanePisomky");
-    const file = vlozenePisomky.files[0];
-    
-    if (file) {
-        const reader = new FileReader();
+    if (!document.getElementById("priestorVsetkychOtazok").contains(document.getElementById("textCelySubor"))) { // spracuje a prida text len v pripade, že tam este zoznam neni
+        const vlozenePisomky = document.getElementById("zadanePisomky");
+        const file = vlozenePisomky.files[0];
+        
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(event) {
+                const textVSubore = event.target.result;
+                const textContainer = document.getElementById("priestorVsetkychOtazok");
+                const extrahovanyText = document.createElement("pre");
+                extrahovanyText.classList.add("textVOtazkovychRamcoch");
+                extrahovanyText.id = "textCelySubor";
+                extrahovanyText.textContent = textVSubore;
+                textContainer.appendChild(extrahovanyText);
+            };
 
-        reader.onload = function(event) {
-            var textInsideFile = event.target.result;
-            document.getElementById()
-        };
+            reader.readAsText(file);
 
-        reader.readAsText(file)
-
-    } else {
-        alert("Zvolte .txt subor")
+        } else {
+            alert("Zvolte .txt subor");
+        }
     }
-
 }
 
 
@@ -24,15 +29,26 @@ function shuffleArray(arr) {
         const j = Math.floor(Math.random() * (i + 1));
         [arr[i], arr[j]] = [arr[j], arr[i]];
     }   
+    return arr;
 }
 
-function selectRandom(numberOfQuestions) {
-    const questions = document.getElementById("obsahSuboru").innerText;
-    //alert(questions)
-    const container = document.getElementById("priestorOtazok")
+function selectRandom() {
+    const container = document.getElementById("priestorVybranychOtazok"); // priestor pre vlozenie otazok
+    if (container.contains(document.getElementById("textJednotliveOtazky"))) { // v pripade opakovanej generacie otazok treba zmazat predosle
+        document.getElementById("textJednotliveOtazky").remove()
+    }
+    var numberOfQuestions = +document.getElementById("zadaniePoctuOtazok").value; // pocet otazok extrahovany z <input> elementu
+    const questions = document.getElementById("textCelySubor").innerText; // ulozenie textu na spracovanie 
+    const questionsArray = questions.split("\n") // premena textu na zoznam
+    // cast na generovanie nahodneho poradia
+    var shuffledArray = shuffleArray(questionsArray) // nahodne zoradeny zoznam otazok
+    var selectedQuestions = shuffledArray.slice(0, numberOfQuestions+1); // var namiesto const pre pripad generovania viackrat
+    var formatedQuestions = selectedQuestions.join("\n") // spojit zoznam do stringu
 
-    const selectedQuestions = questions.slice(0, numberOfQuestions);
-    const testElement = document.createElement("div");
-    testElement.classList.add("vybranaOtazka")
-    container.appendChild(testElement)
+    // pridanie otazok do priestoru pre vygenerovanu pisomku
+    const testElement = document.createElement("pre"); // vytvoreny novy prvok
+    testElement.classList.add("textVOtazkovychRamcoch"); // pridanie potrebnych tried
+    testElement.id = "textJednotliveOtazky"
+    testElement.innerText = formatedQuestions; // vlozenie textu s otazkami
+    container.appendChild(testElement); // nasledne pridanie do ramca
 }
